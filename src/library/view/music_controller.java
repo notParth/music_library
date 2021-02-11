3db4a4fd44d6047fd9fd03616a5fb8a01909d109
notParth <40437768+notParth@users.noreply.github.com>
2021-02-11 02:50:59 -0500

    added new buttons and text fields

--- src/library/view/music_controller.java
+++ src/library/view/music_controller.java
@@ -2,10 +2,13 @@ package library.view;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
 
 public class music_controller {
     @FXML
@@ -16,12 +19,45 @@ public class music_controller {
     Button new_song;
     @FXML
     Button delete_song;
+    @FXML
+    TextField song_name;
+    @FXML
+    TextField artist_name;
+    @FXML
+    TextField album_name;
+    @FXML
+    TextField song_year;
 
     private ObservableList<String> obsList;
+    private String song = "";
+    private int index = -1;
 
     public void start(Stage mainStage){
         obsList = FXCollections.observableArrayList();
         list_view.setItems(obsList);
         list_view.getSelectionModel().select(0);
+        list_view.getSelectionModel().selectedIndexProperty().addListener(
+                (obs, oldVal, newVal) -> storeItemDetails(mainStage)
+        );
+    }
+
+    public void add(ActionEvent e){
+        String song = song_name.getText();
+        String artist = artist_name.getText();
+        String album = album_name.getText();
+        String year = song_year.getText();
+
+    }
+    public void delete(ActionEvent e){
+
+    }
+    public void edit(ActionEvent e){
+
+    }
+
+    private void storeItemDetails(Stage mainStage) {
+        song = list_view.getSelectionModel().getSelectedItem();
+        index = list_view.getSelectionModel().getSelectedIndex();
+
     }
 }
